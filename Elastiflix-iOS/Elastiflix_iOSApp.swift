167abe60174c3de0351dc72b58a63c2e5b44469d
David Hope <david.hope@elastic.co>
2023-07-27 08:55:19 -0400

    Initial Commit

---
+++ Elastiflix-iOS/Elastiflix_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elastiflix_iOSApp.swift
+//  Elastiflix-iOS
+//
+//  Created by David Hope on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Elastiflix_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
